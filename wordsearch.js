2b81e628b603b74fc9cc1d87382f52aa344ba7a1
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-26 17:34:59 -0500

    edited word search file and added extra tests to test_wordsearch file

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
-}
+
+    for (let col = 0; col < letters[0].length; col++) {
+        let colString = '';
+        for (let row = 0; row < letters.length; row++) {
+            colString += letters[row][col];
+        }
+        if (colString.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+};
 
 module.exports = wordSearch
